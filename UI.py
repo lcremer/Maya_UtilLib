32c06ab80ac7556172552fb01195b656548d6eb9
lcremer <leonard.cremer>
2018-11-30 16:23:56 -0800

    Removed obsolete getMayaWindow utility function

--- UI.py
+++
@@ -1,8 +0,0 @@
-from PySide2 import QtWidgets
-import maya.OpenMayaUI as mui
-import shiboken2
-
-
-def getMayaWindow():
-    pointer = mui.MQtUtil.mainWindow()
-    return shiboken2.wrapInstance(long(pointer), QtWidgets.QMainWindow)
