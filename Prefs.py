7fb47cad73fffa700606d16124ce33ea32f301b5
lcremer <leonard.cremer>
2019-01-15 22:38:58 -0800

    Setup Prefs module to appropriately set size of grid with unit change

--- Prefs.py
+++ Prefs.py
@@ -1,13 +1,36 @@
 import pymel.core as pmc
 
 
+def print_grid():
+    g_size = pmc.grid(q=True, size=True)
+    g_spacing = pmc.grid(q=True, spacing=True)
+    g_divisions = pmc.grid(q=True, divisions=True)
+
+    print "size: " + str(g_size)
+    print "spacing: " + str(g_spacing)
+    print "divisions: " + str(g_divisions)
+
+
+def get_grid():
+    g_size = pmc.grid(q=True, size=True)
+    g_spacing = pmc.grid(q=True, spacing=True)
+    g_divisions = pmc.grid(q=True, divisions=True)
+    return [g_size, g_spacing, g_divisions]
+
+
+def set_unit(unit, near_clip, far_clip):
+    pmc.currentUnit(linear=unit)
+    pmc.setAttr('perspShape.nearClipPlane', near_clip)
+    pmc.setAttr('perspShape.farClipPlane', far_clip)
+
+
 def set_meters():
-    pmc.currentUnit(linear='meter')
-    pmc.setAttr('perspShape.nearClipPlane', 0.1)
-    pmc.setAttr('perspShape.farClipPlane', 1000.0)
+    g_props = get_grid()
+    set_unit('m', 0.1, 1000.0)
+    pmc.grid(size=g_props[0], spacing=g_props[1], divisions=g_props[2])
 
 
 def set_centimeters():
-    pmc.currentUnit(linear='cm')
-    pmc.setAttr('perspShape.nearClipPlane', 0.001)
-    pmc.setAttr('perspShape.farClipPlane', 10.0)
+    g_props = get_grid()
+    set_unit('cm', 0.001, 10.0)
+    pmc.grid(size=g_props[0], spacing=g_props[1], divisions=g_props[2])
